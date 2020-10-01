2da09fed14d044366472130da229e6888d34f42e
Arisa Chue <arisachue@gmail.com>
2020-10-01 14:04:48 -0400

    Initial commit, send a simple volley request

---
+++ volley/app/src/main/java/com/example/volley/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        fetchJsonResponse();
+    }
+
+    private void fetchJsonResponse() {
+        String url = "http://ip.jsontest.com/";
+        // Pass second argument as "null" for GET requests
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String result = "Your IP Address is " + response.getString("ip");
+
+                            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        error.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /* Add your Requests to the RequestQueue to execute */
+        mRequestQueue.add(req);
+    }
+}
