060c0d51d57fd1edf82ecf4770028f88657f3b35
ac-nk <arisachue@gmail.com>
2020-11-05 14:17:31 -0500

    Initial commit, take in user data and send with volley

---
+++ Input/app/src/main/java/com/example/input/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.input;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    String termsString;
+
+    private RequestQueue mRequestQueue;
+    EditText mEditText;
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        mButton = findViewById(R.id.analyze);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mEditText = findViewById(R.id.terms);
+                termsString = mEditText.getText().toString();
+
+                volley();
+            }
+        });
+    }
+
+    private void volley(){
+
+        String url = "http://httpbin.org/post";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", "error");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+
+                return params;
+            }
+        };
+        mRequestQueue.add(postRequest);
+    }
+}
